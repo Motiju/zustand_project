a7dc249077961dfd850a63554d456fe928a34885
moti <moti041001@gmail.com>
2026-03-02 10:04:11 +0200

    finish the server and make save

--- server/server.js
+++ server/server.js
@@ -2,30 +2,17 @@ import { promises as fs } from "fs";
 import express from "express";
 import cors from "cors";
 
-console.log("THIS IS THE FILE RUNNING");
-
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", (req, res) => {
-    console.log("TEST ROUTE HIT");
-    res.send("test works");
-})
-
 app.get("/get_products", async (req, res) => {
     try {
-        console.log(2);
         const allProducts = await fs.readFile("./products.json")
-        console.log(allProducts);
         res.json(JSON.parse(allProducts))
     } catch (error) {
         res.send(`the problem is in get_products: ${error}`)
     }
-})
-
-app.get("/", (req, res) => {
-    res.send("hello")
 })
 
 app.listen(3000, () => {
